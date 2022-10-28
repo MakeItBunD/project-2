5b6e103e1d1d7c05bd8ac1b44ff0d8cbde4f1404
MakeItBunD <36636441+MakeItBunD@users.noreply.github.com>
2022-10-28 18:16:09 +0300

    Delete scripts.js

--- scripts.js
+++
@@ -1,7 +0,0 @@
-const btnOk = document.querySelector('.intro__play');
-const wrapperVideo = document.getElementById('.intro__video');
-
-window.onload=btnOk.addEventListener('click',function(){
-  wrapperVideo.play();
-});
-
